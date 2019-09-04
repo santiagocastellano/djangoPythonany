4f5786703b1a09a46ab969552fc1eedfdc958bbe
santiago <scastellano10@gmail.com>
2019-09-04 12:38:20 -0300

    primer commit

--- djangoPythonany/settings.py
+++ djangoPythonany/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'iaj5p43-wg5)7e++h(e%)@8!ty!i*-b&2dohpfx#syktq8q4*o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'primera',
 ]
 
 MIDDLEWARE = [
